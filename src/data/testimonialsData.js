478aeeee4d141b0de91e7865c7182eff133aad3d
Task-Over <157397575+Task-Over@users.noreply.github.com>
2024-04-22 00:48:13 +0530

    Update testimonialsData.js

--- src/data/testimonialsData.js
+++ src/data/testimonialsData.js
@@ -5,7 +5,7 @@ import image3 from "../assets/image_t3.png";
 export const testimonialsData = [
   {
     image: image1,
-    review: "Choosing Fitclub and selecting the appropriate plan and program was the correct decision for me, as it has enabled me to attain my desired physique.",
+    review: "Choosing this Gym and selecting the appropriate plan and program was the correct decision for me, as it has enabled me to attain my desired physique.",
     name: 'Sahil Khan',
     status : 'ENTREPRENEUR'
   },
